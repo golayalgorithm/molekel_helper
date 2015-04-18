691b1f2633f3c27ef50814b6f5636e7a4b571210
bvuong <bvuong@dunnsolutions.com>
2015-04-18 00:13:31 -0400

    attemping to make this work

--- molekel_help/molekel_help.cpp
+++ molekel_help/molekel_help.cpp
@@ -8,8 +8,21 @@
 
 #include <fstream>
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+
+struct atom {
+	string name;
+	double x;
+	double y;
+	double z;
+};
+
+struct frame {
+	vector<atom> molecule;
+};
+
 
 
 /**
@@ -18,15 +31,53 @@ using namespace std;
 int main(int argc, char** argv) {
 
 	// get file name
-	//char* inputFileName = argv[1];
+	char* inputFileName = argv[1];
 
 	// now we got the name, get the actual file contents to read
-	//ifstream inputFile;
-	//inputFile.open(inputFileName);
+	ifstream inputFile;
+	inputFile.open(inputFileName);
 
 	// alright, let's read the contents
 
-	cout << "hello" << endl;
+	vector<vector<atom> > molecules;
+
+	string input;
+	if (inputFile.is_open()) {
+		int tempInt;
+		string tempString;
+		double tempDouble;
+		string::size_type sz;
+		while (!inputFile.eof()) {
+
+			inputFile >> tempString;
+			int size = stoi(tempString,&sz);//tempInt; // should be 77
+			inputFile >> tempString;
+			inputFile >> tempString;
+			inputFile >> tempString;
+			vector<atom> molecule(size);
+			cout << "scanning atoms" << endl;
+			for (int i=0; i<size; i++) {
+				atom a;
+				inputFile >> a.name;
+				inputFile >> tempString; a.x = stod(tempString,&sz);
+				inputFile >> tempString; a.y = stod(tempString,&sz);
+				inputFile >> tempString; a.z = stod(tempString,&sz);
+				molecule[i] = a;
+				cout << a.name << endl;
+			}
+			cout << "add molecule" << endl;
+			molecules.push_back(molecule);
+			cout << "size: " << molecules.size() << endl;
+		}
+		cout << "ok" << endl;
+	}
+	inputFile.close();
+	atom a = molecules[0][0];
+	cout << "name: " << a.name << endl;
+	cout << "x: " << a.x << endl;
+	cout << "y: " << a.y << endl;
+	cout << "z: " << a.z << endl;
+
 
 	return 0;
 }
